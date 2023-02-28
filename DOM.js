e93b6d06f0eb375e86b7ad2f99b05d02ff97496d
Wilsvn <wilsonvposcablo@gmail.com>
2023-02-28 10:12:32 -0500

    w

--- DOM.js
+++
@@ -1,9 +0,0 @@
-const DOM = {
-  rockButton: document.querySelector("rockButton"),
-  paperButton: document.querySelector("paperButton"),
-  scissorButton: document.querySelector("scissorButton"),
-  opponent: document.querySelector("opponent"),
-  choiceButton: document.querySelector("choice"),
-};
-
-export { DOM };
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import { DOM } from "./DOM";
-import { choices } from "./choices";
+const choices = [
+  {
+    name: "rock",
+    icon: "✊",
+    beats: "scissor",
+  },
+  {
+    name: "scissor",
+    icon: "✌",
+    beats: "paper",
+  },
+  {
+    name: "paper",
+    icon: "✋",
+    beats: "rock",
+  },
+];
 
-DOM();
-choices();
+const DOM = {
+  rockButton: document.querySelector(".rockButton"),
+  paperButton: document.querySelector(".paperButton"),
+  scissorButton: document.querySelector(".scissorButton"),
+  opponent: document.querySelector(".opponent"),
+  choiceButton: document.querySelector(".choice"),
+  winner: document.querySelector(".winner"),
+};
 
 DOM.choiceButton.addEventListener(
   "click",
