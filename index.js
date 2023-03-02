8ae2d5a6da769c9b15702c421235fbfed2615969
Wilsvn <wilsonvposcablo@gmail.com>
2023-03-01 22:56:26 -0500

    w

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const choices = [
+/* const choices = [
   {
     name: "rock",
     icon: "✊",
@@ -15,6 +15,7 @@ const choices = [
     beats: "rock",
   },
 ];
+console.log("yes")
 
 const DOM = {
   rockButton: document.querySelector(".rockButton"),
@@ -25,10 +26,46 @@ const DOM = {
   winner: document.querySelector(".winner"),
 };
 
-DOM.choiceButton.addEventListener(
-  "click",
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  },
-  console.log(getRandomInt(3))
-);
+DOM.rockButton.addEventListener("click", function (clear) {
+  console.log("yes");
+}); */
+
+// Game variables
+let playerScore = 0;
+let computerScore = 0;
+
+// Get HTML elements
+const playerScoreSpan = document.getElementById("player-score");
+const computerScoreSpan = document.getElementById("computer-score");
+const resultDiv = document.getElementById("result");
+const resetButton = document.getElementById("reset");
+
+// Function to get computer's choice
+function getComputerChoice() {
+	const choices = ["rock", "paper", "scissors"];
+	const randomIndex = Math.floor(Math.random() * 3);
+	return choices[randomIndex];
+}
+
+// Function to determine the winner
+function getWinner(playerChoice, computerChoice) {
+	if (playerChoice === computerChoice) {
+		return "It's a tie!";
+	} else if (
+		(playerChoice === "rock" && computerChoice === "scissors") ||
+		(playerChoice === "paper" && computerChoice === "rock") ||
+		(playerChoice === "scissors" && computerChoice === "paper")
+	) {
+		playerScore++;
+		playerScoreSpan.innerHTML = playerScore;
+		return "You win!";
+	} else {
+		computerScore++;
+		computerScoreSpan.innerHTML = computerScore;
+		return "Computer wins!";
+	}
+}
+
+// Function to display the result
+function showResult(result) {
+	resultDiv.innerHTML = result}
