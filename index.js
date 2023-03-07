e3c8fbe65822cbbbd740160f816b3f3ccd0fdbae
Wilsvn <wilsonvposcablo@gmail.com>
2023-03-06 23:43:07 -0500

    w

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-/* const choices = [
+
+const choices = [
   {
     name: "rock",
     icon: "✊",
@@ -16,21 +16,6 @@
     beats: "rock",
   },
 ];
-console.log("yes")
-
-const DOM = {
-  rockButton: document.querySelector(".rockButton"),
-  paperButton: document.querySelector(".paperButton"),
-  scissorButton: document.querySelector(".scissorButton"),
-  opponent: document.querySelector(".opponent"),
-  choiceButton: document.querySelector(".choice"),
-  winner: document.querySelector(".winner"),
-};
-
-DOM.rockButton.addEventListener("click", function (clear) {
-  console.log("yes");
-}); */
-
 
 let playerScore = 0;
 let computerScore = 0;
@@ -53,35 +38,6 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorButton = document.getElementById("scissors");
 
-<<<<<<< HEAD
-
-function getComputerChoice() {
-	const choices = ["rock", "paper", "scissors"];
-	const randomIndex = Math.floor(Math.random() * 3);
-	return choices[randomIndex];
-}
-
-
-function getWinner(playerChoice, computerChoice) {
-	if (playerChoice === computerChoice) {
-		return "It's a tie!";
-	} else if (
-		(playerChoice === "rock" & computerChoice === "scissors"),
-		(playerChoice === "paper" & computerChoice === "rock"),
-		(playerChoice === "scissors" & computerChoice === "paper")
-	) {
-		playerScore+1;
-		playerScoreSpan.innerHTML = playerScore;
-		return "You win!";
-	} else {
-		computerScore+1;
-		computerScoreSpan.innerHTML = computerScore;
-		return "Computer wins!";
-	}
-}
-
-
-=======
 rockButton.addEventListener("click", function getPlayerChoice() {
   let playerChoice = "rock";
   console.log(playerChoice);
@@ -113,16 +69,12 @@ function getWinner(playerChoice, computerChoice) {
     (playerChoice === "scissors") & (computerChoice === "paper"))
   ) {
     playerScore + 1;
-    playerScorePlace.innerHTML = playerScore;
+    playerScorePlace.innerHTML = (playerScore, choices.icon);
     return "Wilson Wins";
   } else {
     computerScore + 1;
-    computerScorePlace.innerHTML = computerScore;
+    computerScorePlace.innerHTML = (computerScore, choices.icon);
     return "Opponent Wins";
   }
 }
 
->>>>>>> ff2108970c4e63e81d9495950399b085c3a3d9d4
-function showResult(result) {
-  result.innerHTML = result;
-}
