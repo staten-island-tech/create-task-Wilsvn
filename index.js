36da738e6d24789fdf5fffeae405b1b3ed5c7eba
Wilsvn <wilsonvposcablo@gmail.com>
2023-03-06 21:37:58 -0500

    w

--- index.js
+++ index.js
@@ -30,42 +30,42 @@ DOM.rockButton.addEventListener("click", function (clear) {
   console.log("yes");
 }); */
 
-// Game variables
+
 let playerScore = 0;
 let computerScore = 0;
 
-// Get HTML elements
+
 const playerScoreSpan = document.getElementById("player-score");
 const computerScoreSpan = document.getElementById("computer-score");
 const resultDiv = document.getElementById("result");
 const resetButton = document.getElementById("reset");
 
-// Function to get computer's choice
+
 function getComputerChoice() {
 	const choices = ["rock", "paper", "scissors"];
 	const randomIndex = Math.floor(Math.random() * 3);
 	return choices[randomIndex];
 }
 
-// Function to determine the winner
+
 function getWinner(playerChoice, computerChoice) {
 	if (playerChoice === computerChoice) {
 		return "It's a tie!";
 	} else if (
-		(playerChoice === "rock" && computerChoice === "scissors") ||
-		(playerChoice === "paper" && computerChoice === "rock") ||
-		(playerChoice === "scissors" && computerChoice === "paper")
+		(playerChoice === "rock" & computerChoice === "scissors"),
+		(playerChoice === "paper" & computerChoice === "rock"),
+		(playerChoice === "scissors" & computerChoice === "paper")
 	) {
-		playerScore++;
+		playerScore+1;
 		playerScoreSpan.innerHTML = playerScore;
 		return "You win!";
 	} else {
-		computerScore++;
+		computerScore+1;
 		computerScoreSpan.innerHTML = computerScore;
 		return "Computer wins!";
 	}
 }
 
-// Function to display the result
+
 function showResult(result) {
 	resultDiv.innerHTML = result}
